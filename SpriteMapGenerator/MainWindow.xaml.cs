5cc71283507bd7516514b2ff716f5da81b6e2100
lukemonaghan <luke.p.monaghan@gmail.com>
2013-09-16 12:29:11 +1000

    basics

--- SpriteMapGenerator/MainWindow.xaml.cs
+++ SpriteMapGenerator/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace SpriteMapGenerator
 {
@@ -19,28 +20,71 @@ namespace SpriteMapGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class ListViewItem
+        {
+            public string sName { get; set; }
+            public string sPath { get; set; }
+            public Image imgImage   { get; set; }
+            public ListViewItem(string s_Name,string s_Path, Image img_Image)
+            {
+                sName = s_Name;
+                sPath = s_Path;
+                imgImage = img_Image;
+            }
+        }   
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Menu_Load_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Menu_Load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofFile = new OpenFileDialog();
+            ofFile.Filter = "All Image Files|*.jpg; *.bmp; *.png;";
+            ofFile.Multiselect = true;
+            ofFile.RestoreDirectory = true;
+            int i = 0;
+            Nullable<bool> ofFileResult = ofFile.ShowDialog();
+            if (ofFileResult == true)
+            {
+                foreach (String file in ofFile.FileNames)
+                {
+                    BitmapImage bmimg = new BitmapImage();
+                    bmimg.BeginInit();
+                    bmimg.UriSource = new Uri(file);
+                    bmimg.EndInit();
+
+                    Image img = new Image();
+                    img.BeginInit();
+                    img.Source = bmimg;
+                    img.Width = bmimg.Width;
+                    img.Height = bmimg.Height;
+                    img.EndInit();
+
+                    String imagename = ofFile.SafeFileNames[i].Substring(0, ofFile.SafeFileNames[i].Length - System.IO.Path.GetExtension(file).Length);
+
+                    ListViewItem item = new ListViewItem(imagename, ofFile.SafeFileNames[i], img);
+                    LV_Sprites.Items.Add(item);
+
+                    canvasSpriteSheet.Children.Add(img);
+                    i++;
+                }
+            }
+
+        }
+        private void Menu_Save_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private void Menu_Save_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Menu_Exit_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private void Menu_Exit_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Menu_XML_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private void Menu_XML_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-        private void Menu_pList_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Menu_pList_Click(object sender, RoutedEventArgs e)
         {
 
         }
