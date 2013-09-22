a110e43347c8321ebe338de120dabf66f3095d58
lukemonaghan <luke.p.monaghan@gmail.com>
2013-09-22 20:27:56 +1000

    Weekend work

--- SpriteMapGenerator/MainWindow.xaml.cs
+++ SpriteMapGenerator/MainWindow.xaml.cs
@@ -15,36 +15,43 @@ using Microsoft.Win32;
 
 namespace SpriteMapGenerator
 {
+    public class ListViewItem
+    {
+        public string sName { get; set; }
+        public string sPath { get; set; }
+        public Image imgImage { get; set; }
+        public ListViewItem(string s_Name, string s_Path, Image img_Image)
+        {
+            sName = s_Name;
+            sPath = s_Path;
+            imgImage = img_Image;
+        }
+    } 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public double offx = 0,offy = 0;
-        bool bPressed = false;
-        int SnapValX = 8;
-        int SnapValY = 8;
-        Point MousePoint;
-        Image CurrentImage = null;
-        Window1 winSettings = null;
-        Point Snap;
-        public class ListViewItem
-        {
-            public string sName     { get; set; }
-            public string sPath     { get; set; }
-            public Image imgImage   { get; set; }
-            public ListViewItem(string s_Name,string s_Path, Image img_Image)
-            {
-                sName = s_Name;
-                sPath = s_Path;
-                imgImage = img_Image;
-            }
-        }   
+
+        private double offx = 0,offy = 0;
+        private bool bPressed = false;
+
+        private int SnapValX = 0;           public int SetSnapValX  { set { SnapValX = value; Label_SnapValX.Content = SnapValX; } }
+        private int SnapValY = 0;           public int SetSnapValY  { set { SnapValY = value; Label_SnapValY.Content = SnapValY; } }
+        private bool SnapX = false;         public  bool SetSnapX   { set { SnapX    = value; } }
+        private bool SnapY = false;         public bool SetSnapY { set { SnapY = value; } }
+        private double MaxWidth = 1024.0;   public double SetMaxWidth { set { MaxWidth = value; } }
+        private double MaxHeight = 1024.0;  public double SetMaxHeight { set { MaxHeight = value; } }
+
+        private Point MousePoint;
+        private Image CurrentImage = null;
+        private Window SettingsWindowV;
+        private Point Snap;
         //Main
         public MainWindow()
         {
             InitializeComponent();
-            CheckSize();
+            CheckSize(0.0, 0.0);
             Snap = new Point();
             Label_SnapValX.Content = SnapValX;
             Label_SnapValY.Content = SnapValY;
@@ -74,23 +81,23 @@ namespace SpriteMapGenerator
                     bmimg.EndInit();
                     //Generate the Image
                     Image img = new Image();
+
                     img.BeginInit();
                     img.Source = bmimg;
-                    img.Width = bmimg.Width;
-                    img.Height = bmimg.Height;
+                    img.Width = bmimg.PixelWidth;
+                    img.Height = bmimg.PixelHeight;
                     img.SetValue(Canvas.LeftProperty, offx);
                     img.SetValue(Canvas.TopProperty, offy);
                     img.EndInit();
+
                     //Update the OffX/OffY
-                    offx += img.Width;
-                    offy = img.Height;
-                    CheckSize();
+                    CheckSize(img.Width, img.Height);
                     //Setup the ShortName
                     String imagename = ofFile.SafeFileNames[i].Substring(0, ofFile.SafeFileNames[i].Length - System.IO.Path.GetExtension(file).Length);
                     //Create the ListViewItem
                     ListViewItem item = new ListViewItem(imagename, ofFile.SafeFileNames[i], img);
                     //Add it to everything
-                    LV_Layers.Items.Add(item);
+                    LV_Layers.Items.Insert(0, item);
                     LV_Sprites.Items.Add(item);
                     canvasSpriteSheet.Children.Add(img);
                     //Goto the next ID
@@ -98,30 +105,29 @@ namespace SpriteMapGenerator
                 }
             }
         }
+        private void Menu_New_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want wipe everything?", "Wipe Canvas", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                ClearAll();
+            }
+        }
         private void Menu_Exit_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            if (MessageBox.Show("Are you sure you want to exit?","Exit Sprite Map Generator", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
         }
         private void Menu_Settings_Click(object sender, RoutedEventArgs e)
         {
-            winSettings = new Window1();
-            winSettings.Show();
+            SettingsWindowV = new SettingsWindow(SnapValX,SnapValY,SnapX,SnapY,MaxWidth,MaxHeight);
+            SettingsWindowV.Show();
         }
         private void Menu_XML_Click(object sender, RoutedEventArgs e)
         {
-            //PngBitmapEncoder PNGEncode = new PngBitmapEncoder();
-            foreach (Image Item in canvasSpriteSheet.Children)
-            {
-                //TODO - Exporting the canvas and XML file
-            }
-        }
-        private void Menu_pList_Click(object sender, RoutedEventArgs e)
-        {
-            //PngBitmapEncoder PNGEncode = new PngBitmapEncoder();
-            foreach (Image Item in canvasSpriteSheet.Children)
-            {
-                //TODO - Exporting the canvas and pList file
-            }
+            //Export the Canvas to XML
+            //Export the Canvas to PNG
         }
         //check what image we are pressing
         private void LV_Sprites_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -129,33 +135,33 @@ namespace SpriteMapGenerator
             ListViewItem vClicked = null;
             if (LV_Sprites.Items.Count > 0)
             {
-                vClicked = LV_Sprites.Items.GetItemAt(0) as ListViewItem;
+                vClicked = LV_Sprites.SelectedItem as ListViewItem;
             }
             //TODO
             if (vClicked != null)
             {
                 Image img = new Image();
+
                 img.BeginInit();
                 img.Source = vClicked.imgImage.Source;
                 img.Width = vClicked.imgImage.Width;
                 img.Height = vClicked.imgImage.Height;
                 img.SetValue(Canvas.LeftProperty, 0.0);
-                img.SetValue(Canvas.TopProperty, img.Height);
+                img.SetValue(Canvas.TopProperty, 0.0);
                 img.EndInit();
+
                 canvasSpriteSheet.Children.Add(img);
-                offy = img.Height;
-                CheckSize();
+                CheckSize(img.Width,img.Height);
                 LV_Layers.Items.Add(new ListViewItem(vClicked.sName, vClicked.sPath, vClicked.imgImage));
             }
         }
         //moving the canvas around
         private void CanvasViewport_MouseMove(object sender, MouseEventArgs e)
         {
-            if (Keyboard.IsKeyDown(Key.Space) && bPressed)
+            if (bPressed)
             {
                 canvasSpriteSheet.SetValue(Canvas.LeftProperty, e.MouseDevice.GetPosition(this).X - MousePoint.X);
                 canvasSpriteSheet.SetValue(Canvas.TopProperty, e.MouseDevice.GetPosition(this).Y - MousePoint.Y);
-
             }
         }
         private void CanvasViewport_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
@@ -170,10 +176,10 @@ namespace SpriteMapGenerator
         //Moving sprites on the canvas
         private void canvasSpriteSheet_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Point Mouse = e.MouseDevice.GetPosition(canvasSpriteSheet);
+            MousePoint = e.MouseDevice.GetPosition(canvasSpriteSheet);
             foreach (Image Item in canvasSpriteSheet.Children)
             {
-                if (CheckInside(Mouse, Item))
+                if (CheckInside(MousePoint, Item))
                 {
                     CurrentImage = Item;
                 }
@@ -185,17 +191,63 @@ namespace SpriteMapGenerator
         }
         private void canvasSpriteSheet_MouseMove(object sender, MouseEventArgs e)
         {
-            if (CurrentImage != null)
-            {
-                Snap.X = (int)(e.MouseDevice.GetPosition(canvasSpriteSheet).X - CurrentImage.Width * 0.5)  / SnapValX * SnapValX;
-                Snap.Y = (int)(e.MouseDevice.GetPosition(canvasSpriteSheet).Y - CurrentImage.Height * 0.5) / SnapValY * SnapValY;
-                CurrentImage.SetValue(Canvas.LeftProperty, Snap.X);
-                CurrentImage.SetValue(Canvas.TopProperty, Snap.Y);
+            if (CurrentImage != null) {
+                Snap.X = (e.MouseDevice.GetPosition(canvasSpriteSheet).X - CurrentImage.Width * 0.5);
+                Snap.Y = (e.MouseDevice.GetPosition(canvasSpriteSheet).Y - CurrentImage.Height * 0.5);
+                if (SnapX)
+                {
+                    Snap.X = (int)Snap.X / SnapValX * SnapValX;
+                } if (SnapY) 
+                {
+                    Snap.Y = (int)Snap.Y / SnapValY * SnapValY;
+                }
+                CurrentImage.SetValue(Canvas.LeftProperty   , Snap.X);
+                CurrentImage.SetValue(Canvas.TopProperty    , Snap.Y);
             }
         }
-        //resizing
-        private void CheckSize()
+        //Changing Layer Depths
+        private void LV_Layers_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            ListViewItem draggedItem = null;
+            if (LV_Sprites.Items.Count > 0)
+            {
+                draggedItem = LV_Layers.SelectedItem as ListViewItem;
+            }
+            if (draggedItem != null)
+            {
+                MessageBox.Show(draggedItem.sName);
+            }
+        }
+        private void LV_Layers_Drop(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("Yeah Dropped");
+        }
+        //resizing
+        private void CheckBounds()
+        {
+            //This will replace CheckSize
+        }
+        private void ClearAll()
+        {
+            canvasSpriteSheet.Children.Clear();
+            LV_Layers.Items.Clear();
+            LV_Sprites.Items.Clear();
+            canvasSpriteSheet.Width = 0.0;
+            Label_ImageWidth.Content = canvasSpriteSheet.Width;
+            canvasSpriteSheet.Height = 0.0;
+            Label_ImageHeight.Content = canvasSpriteSheet.Height;
+            offx = 0.0;
+            offy = 0.0;
+        }
+        private void CheckSize(double Width, double Height)
+        {
+            //Replace this with AutoTiling
+            offx += Width;
+            if (offx >= MaxWidth && offy < MaxHeight)
+            {
+                offx = 0.0;
+                offy += Height;
+            }
             //Width
             if (offx > canvasSpriteSheet.Width)
             {
@@ -208,6 +260,15 @@ namespace SpriteMapGenerator
                 canvasSpriteSheet.Height = Power(offy);
                 Label_ImageHeight.Content = canvasSpriteSheet.Height;
             }
+        }
+        private void CheckCanvas()
+        {
+            //check if there is an image outside the bounds
+            //resize coresponding side
+            //check the size of the canvas is still a Power2
+            //if true continue
+            //move opposites to keep the view in the same place
+            //move other images to keep them in the same "place"
         }
         private int Power(double value)
         {
---
+++ SpriteMapGenerator/SettingsWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpriteMapGenerator
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public int ixVal, iyVal;
+        public bool bSnapX, bSnapY;
+        public double dMaxWidth, dMaxHeight;
+        MainWindow MainWin;
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+
+            MainWin = Application.Current.Windows[0] as MainWindow;
+        }
+        public SettingsWindow(int x, int y, bool xx, bool yy,double MaxW, double MaxH)
+        {
+            InitializeComponent();
+
+            ixVal   = x;    tb_XVal.Text      = x.ToString();
+            iyVal   = y;    tb_YVal.Text      = y.ToString();
+
+            bSnapX = xx;    cb_XVal.IsChecked = xx;
+            bSnapY = yy;    cb_YVal.IsChecked = yy;
+
+            dMaxWidth = MaxW; tb_MaxWidth.Text = MaxW.ToString();
+            dMaxHeight = MaxH; tb_MaxHeight.Text = MaxH.ToString();
+
+            MainWin = Application.Current.Windows[0] as MainWindow;
+        }
+
+        private void cb_XVal_Click(object sender, RoutedEventArgs e)
+        {
+            bSnapX = (bool)cb_XVal.IsChecked;
+            if (MainWin != null)
+            {
+                MainWin.SetSnapX = bSnapX;
+            }
+        }
+        private void cb_YVal_Click(object sender, RoutedEventArgs e)
+        {
+            bSnapY = (bool)cb_YVal.IsChecked;
+            
+            if (MainWin != null)
+            {
+                MainWin.SetSnapY = bSnapY;
+            }
+        }
+        private void tb_XVal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ixVal = int.Parse(tb_XVal.Text.ToString());
+            if (MainWin != null)
+            {
+                MainWin.SetSnapValX = ixVal;
+            }
+        }
+        private void tb_YVal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            iyVal = int.Parse(tb_YVal.Text.ToString());
+            if (MainWin != null)
+            {
+                MainWin.SetSnapValY = iyVal;
+            }
+        }
+
+        private void tb_MaxHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            dMaxHeight = double.Parse(tb_MaxHeight.Text.ToString());
+            if (MainWin != null)
+            {
+                MainWin.SetMaxHeight = dMaxHeight;
+            }
+        }
+
+        private void tb_MaxWidth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            dMaxWidth = double.Parse(tb_MaxWidth.Text.ToString());
+            if (MainWin != null)
+            {
+                MainWin.SetMaxWidth = dMaxWidth;
+            }
+        }
+    }
+}
